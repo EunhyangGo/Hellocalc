5a8a6ac4423fc9d54dea002e1d3bf18058e30ed6
EunhyangGo <hyang2lol@naver.com>
2018-11-20 16:09:16 +0900

    first

---
+++ src/NumberCalc.java
@@ -0,0 +1,15 @@
+
+public class NumberCalc {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+        // 숫자 50과 30의 덧셈, 뺄셈, 곱셈 나눗셈 결과를 콘솔에 표시하는 코드를 작성하기
+		
+		System.out.println("50+30=" + (50+30));
+		System.out.println("50-30=" + (50-30));
+		System.out.println("50*30=" + (50*30));
+		System.out.println("50/30=" + (50/30)); // 나누기(몫)를 구하는 연산
+		System.out.println("50%30=" + (50%30)); //나머지를 구하는 연산, 모둘레이션
+	}
+
+}
